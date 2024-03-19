e3d97cc12ab5a5dfc1bbdce60eff9708d3536211
amisg <aamithsg18@gmail.com>
2024-03-19 20:11:58 +0530

    structural change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import HelloComponent from "./1.Basics/1.HelloComponent/HelloComponent";
+import HelloClassComponent from "./1.Basics/1.HelloComponent/HelloClassComponent";
+import CounterComponent from "./1.Basics/2.CounterComponent/CounterComponent";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		// <div className="App">
+		//   <header className="App-header">
+		//     <img src={logo} className="App-logo" alt="logo" />
+		//     <p>
+		//       Edit <code>src/App.js</code> and save to reload.
+		//     </p>
+		//     <a
+		//       className="App-link"
+		//       href="https://reactjs.org"
+		//       target="_blank"
+		//       rel="noopener noreferrer"
+		//     >
+		//       Learn React
+		//     </a>
+		//   </header>
+		// </div>
+		<>
+			{/* <HelloComponent name={"amith"} />
+			<HelloClassComponent />
+			<HelloClassComponent /> */}
+			<CounterComponent heading={"counter for add and sub"} />
+		</>
+	);
 }
 
 export default App;
